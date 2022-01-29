7610c9a5bcb2da617d1b3e05d4749484f5644b45
Otabek Askarov <65924250+Otabek-coder@users.noreply.github.com>
2022-01-29 20:48:16 +0900

    Update script.js

--- script.js
+++ script.js
@@ -7,20 +7,11 @@ function toggler(){
     let styleDisplay = cnsContainer.style
     if ( styleDisplay.display === "none"){
         styleDisplay.display = "inline-block"
-        // togglerBtn_mob.style.display = "none"
     }else{
         styleDisplay.display = "none"
     } 
 }
-//  function togglerMobileBtn(){
-//      if( togglerBtn_mob.style.display === "none"){
-//         togglerBtn_mob.style.display = "block"
-//         styleDisplay.display = "none"
 
-//      }else{
-//         togglerBtn_mob.style.display = "none"
-//      }
-//  }
 togglerBtn.addEventListener('click', toggler)
 togglerBtn_mob.addEventListener('click', toggler)
 
